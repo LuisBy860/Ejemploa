61927655723f76a26c38bd342bc6c52c90638606
Jose101503 <ph0172032019@unab.edu.sv>
2021-09-18 14:56:29 -0600

    cambios2

--- ConsoleApp1/ConsoleApp1/Negocio/ClsNegocio.cs
+++ ConsoleApp1/ConsoleApp1/Negocio/ClsNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp1.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace ConsoleApp1.Negocio
 {
     class ClsNegocio
     {
+        public int Log (Persona persona)
+        {
+            if (persona.Usuario == "Jose" && persona.Pass == "123")
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
     }
 }
