f3699530a4abf5a64617941ac9a9c024768616ca
Luis Serrano <84140881+LuisBy860@users.noreply.github.com>
2021-09-18 14:30:19 -0600

    goTwo
    Agregando Logica de negocio

---
+++ ConsoleApp1/ConsoleApp1/Entities/Persona.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Entities
+{
+    class Persona
+    {
+        private String usuario;
+        private String pass;
+
+        public string Usuario { get => usuario; set => usuario = value; }
+        public string Pass { get => pass; set => pass = value; }
+
+
+
+    }
+}
